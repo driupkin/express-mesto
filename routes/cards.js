0106952d81160c16828c9001f9d3308810cdefb2
Stas Borisov <driupkin@mail.ru>
2020-09-19 18:19:16 +0300

    project 12 review 3

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const fs = require('fs').promises;
+const path = require('path');
 
 router.get('/cards', (req, res) => {
-  fs.readFile('data/cards.json')
+  fs.readFile(path.join('data', 'cards.json'))
     .then((data) => {
       const cards = JSON.parse(data);
       res.status(200).json(cards);
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,38 @@
 const router = require('express').Router();
-const users = require('../data/users.json');
+const fs = require('fs').promises;
+const path = require('path');
+
+const usersPath = path.join('data', 'users.json');
 
 router.get('/users', (req, res) => {
-  res.send(users);
+  fs.readFile(usersPath)
+    .then((data) => {
+      const users = JSON.parse(data);
+      res.status(200).json(users);
+    })
+    .catch(() => {
+      res.status(500).send({ messege: 'Ошибка чтения файла.' });
+    });
 });
 router.get('/users/:id', (req, res) => {
-  let user = {};
-  const isUserId = users.some((e) => {
-    user = e;
-    return e._id === req.params.id;
-  });
-  if (!isUserId) {
-    res.status(404);
-    res.send({ message: 'Нет пользователя с таким id' });
-    return;
-  }
-  res.send(user);
+  fs.readFile(usersPath)
+    .then((data) => {
+      const users = JSON.parse(data);
+      let user = {};
+      const isUserId = users.some((e) => {
+        user = e;
+        return e._id === req.params.id;
+      });
+      if (!isUserId) {
+        res.status(404);
+        res.send({ message: 'Нет пользователя с таким id' });
+        return;
+      }
+      res.send(user);
+    })
+    .catch(() => {
+      res.status(500).send({ messege: 'Ошибка чтения файла.' });
+    });
 });
 
 module.exports = router;
